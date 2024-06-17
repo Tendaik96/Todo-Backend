ececa5fe31d9f898f01a728b07006f2ea9a49be1
Tendai Karuwo <tendaikaruwo96@gmail.com>
2024-06-17 17:30:01 +0100

    added create todo functionality

--- models/todos.ts
+++ models/todos.ts
@@ -3,6 +3,8 @@ const prisma = new PrismaClient();
 
 // its always a good idea to disconnect from the database as soon as you program finishes running
 
+
+//Get all Todos
 async function getAllTodo() {
     const todo = await prisma.todo.findMany()
     console.log(todo)
@@ -16,5 +18,19 @@ getAllTodo()
         await prisma.$disconnect()
     })
 
+
+    // Create Todo
+    async function createTodo() {
+    const todo = await prisma.todo.create({ data: { task: "write a blog", progress: "incomplete" } })
+    console.log(todo)
+}
+
+createTodo()
+    .catch(e => {
+        console.error(e.message)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
 
 
